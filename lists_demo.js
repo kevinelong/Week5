0ec92930fb24fb656a2698b44fbf8614ba6209c4
kevinelong <kevinelong@gmail.com>
2023-10-30 09:39:45 -0700

    update by index

--- lists_demo.js
+++ lists_demo.js
@@ -16,6 +16,9 @@ let people = [
 //ADD ONE MORE
 people.push({name: "Kay", age: 59});
 
+people[2].name = "Kate";//update the last person
+people[0].name = "Kevy";//update the first person
+
 // for(START, STOP, HOW)
 for(let index = 0; index < people.length; index++){
     console.log(people[index].name, people[index].age);
