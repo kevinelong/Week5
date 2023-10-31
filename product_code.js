6c0dd411de73a67d28f14677357ed581ae672080
kevinelong <kevinelong@gmail.com>
2023-10-31 13:15:23 -0700

    product_code

---
+++ product_code.js
@@ -0,0 +1,29 @@
+// product_code.js
+function parsePartCode(code){
+    //split on both ':' and '-' using REGEX
+    // const pieces = code.split(/[:-]+/)
+    
+    let replaced = code.replace(":", "-");
+    const pieces = replaced.split("-");
+    
+    // const pieces = code.replace(":", "-").split("-")
+
+    // const pieces = [
+    //     code.split(":")[0],
+    //     code.split(":")[1].split("-")[0],
+    //     code.split("-")[1],
+    // ];
+
+    return {
+        supplierCode: pieces[0],
+        productNumber: pieces[1],
+        size: pieces[2],
+    }
+}
+
+let partCode1 = "XYZ:1234-L";
+let part1 = parsePartCode(partCode1);
+
+console.log("Supplier: " + part1.supplierCode)
+console.log("Product Number: " + part1.productNumber)
+console.log("Size: " + part1.size);
