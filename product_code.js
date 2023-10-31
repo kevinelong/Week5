85ab19c7a14ac8415365fd4061233e8eb837e887
kevinelong <kevinelong@gmail.com>
2023-10-31 13:57:26 -0700

    parse

--- product_code.js
+++ product_code.js
@@ -1,4 +1,3 @@
-// product_code.js
 function parsePartCode(code){
     //split on both ':' and '-' using REGEX
     // const pieces = code.split(/[:-]+/)
@@ -17,17 +16,23 @@ function parsePartCode(code){
     const left = code.split(":");
     const right = left[1].split("-")
     const pieces = [left[0],right[0],right[1]];
-     
+
     return {
         supplierCode: pieces[0],
         productNumber: pieces[1],
         size: pieces[2],
     }
 }
-
+//TESTS
 let partCode1 = "XYZ:1234-L";
 let part1 = parsePartCode(partCode1);
 
 console.log("Supplier: " + part1.supplierCode)
 console.log("Product Number: " + part1.productNumber)
 console.log("Size: " + part1.size);
+
+/*
+Supplier: XYZ
+Product Number: 1234
+Size: L
+*/
